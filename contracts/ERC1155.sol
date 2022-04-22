1ac06f1cdd978f38ed03084307d3fe6b3d4c1658
jsan <ahsan.wtc@gmail.com>
2022-04-22 17:26:56 +0500

    implementing balanceOf function

--- contracts/ERC1155.sol
+++ contracts/ERC1155.sol
@@ -2,16 +2,23 @@
 pragma solidity ^0.8.2;
 
 contract ERC1155 {
-    // event TransferSingle()
-    // event TransferBatch()
-    // event ApprovalForAll()
-    // event URI()
+  /* Mapping from TokenId to account balances */
+  mapping(uint256 => mapping(address => uint256)) internal balances;
 
-    // function balanceOf()
-    // function balanceOfBatch()
-    // function setApprovalForAll()
-    // function safeTransferFrom()
-    // function safeBatchTransferFrom()
-    // function isApprovedForAll()
-    // function supportsInterface()
+  // event TransferSingle()
+  // event TransferBatch()
+  // event ApprovalForAll()
+  // event URI()
+
+  /* Gets the balance of an address's tokens */
+  function balanceOf(address _owner, uint256 _tokenId) public view returns(uint256) {
+    require(_owner != address(0), 'owner address is 0');
+    return balances[_tokenId][_owner];
+  }
+  // function balanceOfBatch()
+  // function setApprovalForAll()
+  // function safeTransferFrom()
+  // function safeBatchTransferFrom()
+  // function isApprovedForAll()
+  // function supportsInterface()
 }
