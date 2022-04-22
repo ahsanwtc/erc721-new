567397e71bafcb5ef6e225b5c7dda274ec8f5567
jsan <ahsan.wtc@gmail.com>
2022-04-22 17:16:05 +0500

    adding scaffolding for erc1155

---
+++ contracts/ERC1155.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract ERC1155 {
+    // event TransferSingle()
+    // event TransferBatch()
+    // event ApprovalForAll()
+    // event URI()
+
+    // function safeTransferFrom() external   
+    // function safeBatchTransferFrom()
+    // function balanceOf()
+    // function balanceOfBatch()
+    // function setApprovalForAll()
+    // function isApprovedForAll()
+}
