fc8ea03e9dcb52bf214eb54371752779b4343214
jsan <ahsan.wtc@gmail.com>
2022-04-23 14:59:51 +0500

    added photo formatting

--- frontend/src/NFTCard.js
+++ frontend/src/NFTCard.js
@@ -4,6 +4,7 @@ const NFTCard = ({ nft }) => {
   console.log(nft);
   return (
     <Card>
+      <Photo style={{ backgroundImage: `url(${nft && nft.image})` }}/>
       This is nft card
     </Card>
   );
@@ -18,6 +19,16 @@ const Card = styled.div`
   box-shadow:  8px 8px 16px #d9d9d9,
              -8px -8px 16px #ffffff;
   cursor: pointer;
+`;
+
+const Photo = styled.div`
+  display: block;
+  width: 200px;
+  height: 200px;
+  background-position: center center;
+  background-size: cover;
+  border-radius: 10px;
+  margin: auto;
 `;
 
 export default NFTCard;
